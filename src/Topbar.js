c83bae1dcb90305153d5e179cc382f187bed6130
JosemaCapgemini <jose-manuel.rodriguez-rabadan@capgemini.com>
2018-11-08 14:17:23 +0100

    Top updated

--- src/Topbar.js
+++ src/Topbar.js
@@ -5,12 +5,13 @@ import './App.css'
 const Topbar = () => {
     return (
         <div className="topnav" id="myTopnav">
-          <a href="http://localhost:3000">Home</a>
-          <a href="#news">News</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/245px-React.svg.png" alt="React logo" height="20" width="20"></img>
+            <a href="http://localhost:3000">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
         </div>
-      );
+    );
 }
 
 export default Topbar;
